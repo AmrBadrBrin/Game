effa5bf87de1c123412c7bf8fa3570ea54782991
AmrBadrBrin <badretdinov.amir@yandex.ru>
2024-05-25 20:54:07 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,43 +1,74 @@
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Middleware для CORS
 app.use(cors());
+app.use(bodyParser.json()); // Middleware для обработки JSON-тел запросов
 
-// Маршрут для получения значения счетчика
-app.get("/clicks", (req, res) => {
-  fs.readFile("clicks.txt", "utf8", (err, data) => {
+// Функция для чтения данных из файла
+function readData(callback) {
+  fs.readFile("data.json", "utf8", (err, data) => {
     if (err) {
       if (err.code === "ENOENT") {
-        return res.send({ clicks: 0 });
+        return callback(null, {}); // Если файл не существует, возвращаем пустой объект
       }
+      return callback(err);
+    }
+    callback(null, JSON.parse(data));
+  });
+}
+
+// Функция для записи данных в файл
+function writeData(data, callback) {
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), callback);
+}
+
+// Маршрут для получения значения счетчика и данных о пользователях
+app.get("/clicks", (req, res) => {
+  readData((err, data) => {
+    if (err) {
       console.error(err);
       return res.status(500).send("Ошибка чтения файла");
     }
-    const clicks = parseInt(data) || 0;
-    res.send({ clicks });
+    const totalClicks = Object.values(data).reduce(
+      (sum, userClicks) => sum + userClicks,
+      0
+    );
+    const results = Object.entries(data).map(([user, clicks]) => ({
+      user,
+      clicks,
+    }));
+    res.send({ clicks: totalClicks, results });
   });
 });
 
-// Маршрут для увеличения значения счетчика
+// Маршрут для увеличения значения счетчика пользователя
 app.post("/clicks", (req, res) => {
-  fs.readFile("clicks.txt", "utf8", (err, data) => {
-    if (err && err.code !== "ENOENT") {
+  const user = req.body.user || "аноним"; // Получаем имя пользователя из запроса, если нет, то "аноним"
+  readData((err, data) => {
+    if (err) {
       console.error(err);
       return res.status(500).send("Ошибка чтения файла");
     }
-    const clicks = parseInt(data) || 0;
-    const newClicks = clicks + 1;
-    fs.writeFile("clicks.txt", newClicks.toString(), (err) => {
+    data[user] = (data[user] || 0) + 1; // Увеличиваем количество нажатий для пользователя
+    writeData(data, (err) => {
       if (err) {
         console.error(err);
         return res.status(500).send("Ошибка записи файла");
       }
-      res.send({ clicks: newClicks });
+      const totalClicks = Object.values(data).reduce(
+        (sum, userClicks) => sum + userClicks,
+        0
+      );
+      const results = Object.entries(data).map(([user, clicks]) => ({
+        user,
+        clicks,
+      }));
+      res.send({ clicks: totalClicks, results });
     });
   });
 });
